07eb7b6dbe7976bae80c009a75898d64d8840836
LUKON1 <leon87953@gmail.com>
2026-02-01 03:09:00 +0300

    optimization && meta tags && Readme

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import { Suspense, lazy } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-import { LibraryPage } from "./pages/LibraryPage";
-import { SkillTreePage } from "./pages/SkillTreePage";
-
-import { TopicPage } from "./pages/TopicPage";
-import { LessonPage } from "./pages/LessonPage";
+// Lazy load pages
+const LibraryPage = lazy(() =>
+	import("./pages/LibraryPage").then((module) => ({ default: module.LibraryPage })),
+);
+const SkillTreePage = lazy(() =>
+	import("./pages/SkillTreePage").then((module) => ({ default: module.SkillTreePage })),
+);
+const TopicPage = lazy(() =>
+	import("./pages/TopicPage").then((module) => ({ default: module.TopicPage })),
+);
+const LessonPage = lazy(() =>
+	import("./pages/LessonPage").then((module) => ({ default: module.LessonPage })),
+);
 
 import { MainLayout } from "./components/layout/MainLayout";
 
@@ -13,13 +21,16 @@ function App() {
 	return (
 		<BrowserRouter>
 			<MainLayout>
-				<Routes>
-					<Route path="/" element={<SkillTreePage />} />
-					<Route path="/library" element={<LibraryPage />} />
-					<Route path="/topic/:id" element={<TopicPage />} />
-					<Route path="/topic/:id/lesson/:lessonId" element={<LessonPage />} />
-					<Route path="*" element={<Navigate to="/" replace />} />
-				</Routes>
+				<Suspense
+					fallback={<div className="flex items-center justify-center h-screen">Загрузка...</div>}>
+					<Routes>
+						<Route path="/" element={<SkillTreePage />} />
+						<Route path="/library" element={<LibraryPage />} />
+						<Route path="/topic/:id" element={<TopicPage />} />
+						<Route path="/topic/:id/lesson/:lessonId" element={<LessonPage />} />
+						<Route path="*" element={<Navigate to="/" replace />} />
+					</Routes>
+				</Suspense>
 			</MainLayout>
 		</BrowserRouter>
 	);
